e8fc1a34771271eb06a05c964d0dc27913261254
Michael Oswald <michael.oswald@onikudaki.net>
2023-05-28 13:38:26 +0200

    lesson 3: with teapot

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::f64::consts::PI;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::num::{ParseFloatError, ParseIntError};
+use std::ops::{Add, Mul, Sub};
 use std::path::Path;
 use std::time::Instant;
 
@@ -33,7 +34,7 @@ pub fn main() {
     let camera = Vec3d::default();
 
     let mut mesh = Mesh::new();
-    match mesh.load_from_file(Path::new("VideoShip.obj")) {
+    match mesh.load_from_file(Path::new("teapot.obj")) {
         Err(err) => {
             println!("Error loading file: {:?}", err);
             return;
@@ -43,18 +44,9 @@ pub fn main() {
 
     let near = 0.1;
     let far = 1000.0;
-    let f_fov = 90.0;
     let aspect_ratio: f64 = HEIGHT as f64 / WIDTH as f64;
-    let fov_rad: f64 = 1.0 / (f_fov * 0.5 / 180.0 * PI).tan() as f64;
 
-    let mut mat_proj = Mat4x4 { m: [[0.0; 4]; 4] };
-
-    mat_proj.m[0][0] = aspect_ratio * fov_rad;
-    mat_proj.m[1][1] = fov_rad;
-    mat_proj.m[2][2] = far / (far - near);
-    mat_proj.m[3][2] = (-far * near) / (far - near);
-    mat_proj.m[2][3] = 1.0;
-    mat_proj.m[3][3] = 0.0;
+    let mat_proj = Mat4x4::make_projection(90.0, aspect_ratio, near, far);
 
     let mut theta: f64 = 0.0;
 
@@ -72,112 +64,65 @@ pub fn main() {
 
         theta += 1.0 * elapsed;
 
-        let mut mat_rot_z = Mat4x4 { m: [[0.0; 4]; 4] };
-        let mut mat_rot_x = Mat4x4 { m: [[0.0; 4]; 4] };
+        let mat_rot_z = Mat4x4::make_rotation_z(theta * 0.5);
+        let mat_rot_x = Mat4x4::make_rotation_x(theta);
 
-        mat_rot_z.m[0][0] = theta.cos();
-        mat_rot_z.m[0][1] = theta.sin();
-        mat_rot_z.m[1][0] = -(theta.sin());
-        mat_rot_z.m[1][1] = theta.cos();
-        mat_rot_z.m[2][2] = 1.0;
-        mat_rot_z.m[3][3] = 1.0;
+        let mat_trans = Mat4x4::make_translation(0.0, 0.0, 8.0);
 
-        mat_rot_x.m[0][0] = 1.0;
-        mat_rot_x.m[1][1] = (theta * 0.5).cos();
-        mat_rot_x.m[1][2] = (theta * 0.5).sin();
-        mat_rot_x.m[2][1] = -((theta * 0.5).sin());
-        mat_rot_x.m[2][2] = (theta * 0.5).cos();
-        mat_rot_x.m[3][3] = 1.0;
+        let mut mat_world = &mat_rot_z * &mat_rot_x;
+        mat_world = &mat_world * &mat_trans;
 
         for tri in &mesh.tris {
-            let tri_rotated_z = Triangle {
+            let tri_transformed = Triangle {
                 p: [
-                    multiply_matrix_vector(&tri.p[0], &mat_rot_z),
-                    multiply_matrix_vector(&tri.p[1], &mat_rot_z),
-                    multiply_matrix_vector(&tri.p[2], &mat_rot_z),
+                    multiply_matrix_vector(&tri.p[0], &mat_world),
+                    multiply_matrix_vector(&tri.p[1], &mat_world),
+                    multiply_matrix_vector(&tri.p[2], &mat_world),
                 ],
-                color: Color::RGB(0, 0, 0),
+                color: Color::RGB(1, 1, 1),
             };
 
-            let tri_rotated_zx = Triangle {
-                p: [
-                    multiply_matrix_vector(&tri_rotated_z.p[0], &mat_rot_x),
-                    multiply_matrix_vector(&tri_rotated_z.p[1], &mat_rot_x),
-                    multiply_matrix_vector(&tri_rotated_z.p[2], &mat_rot_x),
-                ],
-                color: tri_rotated_z.color,
-            };
+            let line1 = tri_transformed.p[1] - &tri_transformed.p[0];
+            let line2 = tri_transformed.p[2] - &tri_transformed.p[0];
 
-            let mut tri_translated = tri_rotated_zx;
+            let normal = line1.cross(&line2).normalise();
 
-            tri_translated.p[0].z = tri_rotated_zx.p[0].z + 8.0;
-            tri_translated.p[1].z = tri_rotated_zx.p[1].z + 8.0;
-            tri_translated.p[2].z = tri_rotated_zx.p[2].z + 8.0;
-
-            let line1 = Vec3d {
-                x: tri_translated.p[1].x - tri_translated.p[0].x,
-                y: tri_translated.p[1].y - tri_translated.p[0].y,
-                z: tri_translated.p[1].z - tri_translated.p[0].z,
-            };
-
-            let line2 = Vec3d {
-                x: tri_translated.p[2].x - tri_translated.p[0].x,
-                y: tri_translated.p[2].y - tri_translated.p[0].y,
-                z: tri_translated.p[2].z - tri_translated.p[0].z,
-            };
-
-            let mut normal = Vec3d {
-                x: line1.y * line2.z - line1.z * line2.y,
-                y: line1.z * line2.x - line1.x * line2.z,
-                z: line1.x * line2.y - line1.y * line2.x,
-            };
-
-            let l = (normal.x * normal.x + normal.y * normal.y + normal.z * normal.z).sqrt();
-            normal.x /= l;
-            normal.y /= l;
-            normal.z /= l;
-
-            let cull = normal.x * (tri_translated.p[0].x - camera.x)
-                + normal.y * (tri_translated.p[0].y - camera.y)
-                + normal.z * (tri_translated.p[0].z - camera.z);
+            let camera_ray = tri_transformed.p[0] - &camera;
+            
+            let cull = normal.dot(&camera_ray);
 
             if cull < 0.0 {
-                let mut light_direction = Vec3d {
+                let light_direction = Vec3d {
                     x: 0.0,
                     y: 0.0,
                     z: -1.0,
-                };
-                let l = (light_direction.x * light_direction.x
-                    + light_direction.y * light_direction.y
-                    + light_direction.z * light_direction.z)
-                    .sqrt();
-                light_direction.x /= l;
-                light_direction.y /= l;
-                light_direction.z /= l;
+                    w: 1.0,
+                }.normalise();
 
-                let dp = normal.x * light_direction.x
-                    + normal.y * light_direction.y
-                    + normal.z * light_direction.z;
-
+                let dp = normal.dot(&light_direction);
                 let gs = ((dp + 1.0) * 0.5 * 255.0) as u8;
-                //let gs = (dp * 255.0) as u8;
 
                 let mut tri_projected = Triangle {
                     p: [
-                        multiply_matrix_vector(&tri_translated.p[0], &mat_proj),
-                        multiply_matrix_vector(&tri_translated.p[1], &mat_proj),
-                        multiply_matrix_vector(&tri_translated.p[2], &mat_proj),
+                        multiply_matrix_vector(&tri_transformed.p[0], &mat_proj),
+                        multiply_matrix_vector(&tri_transformed.p[1], &mat_proj),
+                        multiply_matrix_vector(&tri_transformed.p[2], &mat_proj),
                     ],
                     color: Color::RGB(gs, gs, gs),
                 };
 
+                tri_projected.p[0] = tri_projected.p[0].vec_div(tri_projected.p[0].w);
+                tri_projected.p[1] = tri_projected.p[1].vec_div(tri_projected.p[1].w);
+                tri_projected.p[2] = tri_projected.p[2].vec_div(tri_projected.p[2].w);
+
+                let offset_view = Vec3d {
+                    x: 1.0, y: 1.0, z: 0.0, w: 1.0 
+                };
+
                 // Scale into view.
-                tri_projected.p[0].x += 1.0;
-                tri_projected.p[0].y += 1.0;
-                tri_projected.p[1].x += 1.0;
-                tri_projected.p[1].y += 1.0;
-                tri_projected.p[2].x += 1.0;
-                tri_projected.p[2].y += 1.0;
+                tri_projected.p[0] = tri_projected.p[0] + &offset_view;
+                tri_projected.p[1] = tri_projected.p[1] + &offset_view;
+                tri_projected.p[2] = tri_projected.p[2] + &offset_view;
 
                 tri_projected.p[0].x *= 0.5 * WIDTH as f64;
                 tri_projected.p[0].y *= 0.5 * HEIGHT as f64;
@@ -228,6 +173,7 @@ pub struct Vec3d {
     pub x: f64,
     pub y: f64,
     pub z: f64,
+    pub w: f64,
 }
 
 impl Default for Vec3d {
@@ -236,6 +182,80 @@ impl Default for Vec3d {
             x: 0.0,
             y: 0.0,
             z: 0.0,
+            w: 1.0,
+        }
+    }
+}
+
+impl Vec3d {
+    pub fn vec_mul(&self, k: f64) -> Vec3d {
+        Vec3d {
+            x: self.x * k,
+            y: self.y * k,
+            z: self.z * k,
+            w: 1.0,
+        }
+    }
+
+    pub fn vec_div(&self, k: f64) -> Vec3d {
+        Vec3d {
+            x: self.x / k,
+            y: self.y / k,
+            z: self.z / k,
+            w: 1.0,
+        }
+    }
+
+    pub fn dot(&self, rhs: &Vec3d) -> f64 {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+
+    pub fn len(&self) -> f64 {
+        self.dot(self).sqrt()
+    }
+
+    pub fn normalise(&self) -> Vec3d {
+        let l = self.len();
+        Vec3d {
+            x: self.x / l,
+            y: self.y / l,
+            z: self.z / l,
+            w: 1.0,
+        }
+    }
+
+    pub fn cross(&self, rhs: &Vec3d) -> Vec3d {
+        Vec3d {
+            x: self.y * rhs.z - self.z * rhs.y,
+            y: self.z * rhs.x - self.x * rhs.z,
+            z: self.x * rhs.y - self.y * rhs.x,
+            w: 1.0,
+        }
+    }
+}
+
+impl Add<&Vec3d> for Vec3d {
+    type Output = Vec3d;
+
+    fn add(self, rhs: &Vec3d) -> Self::Output {
+        Vec3d {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+            w: self.w + rhs.w,
+        }
+    }
+}
+
+impl Sub<&Vec3d> for Vec3d {
+    type Output = Vec3d;
+
+    fn sub(self, rhs: &Vec3d) -> Self::Output {
+        Vec3d {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+            w: self.w - rhs.w,
         }
     }
 }
@@ -278,7 +298,7 @@ impl Mesh {
                         let x = fields[1].parse::<f64>()?;
                         let y = fields[2].parse::<f64>()?;
                         let z = fields[3].parse::<f64>()?;
-                        vertices.push(Vec3d { x, y, z });
+                        vertices.push(Vec3d { x, y, z, w: 1.0 });
                     }
                     "f" => {
                         if fields.len() < 4 {
@@ -335,21 +355,127 @@ pub struct Mat4x4 {
     m: [[f64; 4]; 4],
 }
 
-pub fn multiply_matrix_vector(i: &Vec3d, m: &Mat4x4) -> Vec3d {
-    let x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
-    let y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
-    let z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
-    let w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
-
-    if w != 0.0 {
-        Vec3d {
-            x: x / w,
-            y: y / w,
-            z: z / w,
-        }
-    } else {
-        Vec3d { x, y, z }
+impl Default for Mat4x4 {
+    fn default() -> Self {
+        Mat4x4 { m: [[0.0; 4]; 4] }
     }
+}
+
+impl Mat4x4 {
+    pub fn identity() -> Mat4x4 {
+        Mat4x4 {
+            m: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+        }
+    }
+
+    pub fn make_rotation_x(theta: f64) -> Mat4x4 {
+        let mut matrix = Mat4x4::default();
+
+        let cosf = theta.cos();
+        let sinf = theta.sin();
+
+        matrix.m[0][0] = 1.0;
+        matrix.m[1][1] = cosf;
+        matrix.m[1][2] = sinf;
+        matrix.m[2][1] = -sinf;
+        matrix.m[2][2] = cosf;
+        matrix.m[3][3] = 1.0;
+
+        matrix
+    }
+
+    pub fn make_rotation_y(theta: f64) -> Mat4x4 {
+        let mut matrix = Mat4x4::default();
+
+        let cosf = theta.cos();
+        let sinf = theta.sin();
+
+        matrix.m[0][0] = cosf;
+        matrix.m[0][2] = sinf;
+        matrix.m[2][0] = -sinf;
+        matrix.m[1][1] = 1.0;
+        matrix.m[2][2] = cosf;
+        matrix.m[3][3] = 1.0;
+
+        matrix
+    }
+
+    pub fn make_rotation_z(theta: f64) -> Mat4x4 {
+        let mut matrix = Mat4x4::default();
+
+        let cosf = theta.cos();
+        let sinf = theta.sin();
+
+        matrix.m[0][0] = cosf;
+        matrix.m[0][1] = sinf;
+        matrix.m[1][0] = -sinf;
+        matrix.m[1][1] = cosf;
+        matrix.m[2][2] = 1.0;
+        matrix.m[3][3] = 1.0;
+
+        matrix
+    }
+
+    pub fn make_translation(x: f64, y: f64, z: f64) -> Mat4x4 {
+        let mut matrix = Mat4x4::default();
+
+        matrix.m[0][0] = 1.0;
+        matrix.m[1][1] = 1.0;
+        matrix.m[2][2] = 1.0;
+        matrix.m[3][3] = 1.0;
+        matrix.m[3][0] = x;
+        matrix.m[3][1] = y;
+        matrix.m[3][2] = z;
+
+        matrix
+    }
+
+    pub fn make_projection(fov_degress: f64, aspect_ratio: f64, near: f64, far: f64) -> Mat4x4 {
+        let fov_rad = 1.0 / (fov_degress * 0.5 / 180.0 * PI).tan();
+        let far_near = far - near;
+
+        let mut matrix = Mat4x4::default();
+
+        matrix.m[0][0] = aspect_ratio * fov_rad;
+        matrix.m[1][1] = fov_rad;
+        matrix.m[2][2] = far / far_near;
+        matrix.m[3][2] = (-far * near) / far_near;
+        matrix.m[2][3] = 1.0;
+        matrix.m[3][3] = 0.0;
+
+        matrix
+    }
+}
+
+impl Mul<&Mat4x4> for &Mat4x4 {
+    type Output = Mat4x4;
+
+    fn mul(self, rhs: &Mat4x4) -> Self::Output {
+        let mut matrix = Mat4x4::default();
+        for c in 0..4 {
+            for r in 0..4 {
+                matrix.m[r][c] = self.m[r][0] * rhs.m[0][c]
+                    + self.m[r][1] * rhs.m[1][c]
+                    + self.m[r][2] * rhs.m[2][c]
+                    + self.m[r][3] * rhs.m[3][c];
+            }
+        }
+        matrix
+    }
+}
+
+pub fn multiply_matrix_vector(i: &Vec3d, m: &Mat4x4) -> Vec3d {
+    let x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + i.w * m.m[3][0];
+    let y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + i.w * m.m[3][1];
+    let z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + i.w * m.m[3][2];
+    let w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + i.w * m.m[3][3];
+
+    Vec3d { x, y, z, w }
 }
 
 pub fn draw_triangle(canvas: &mut Canvas<Window>, tr: &Triangle) {
